10246be01d9dd2db2600022e9ac7f21475982c0e
Cola_Colin <cola_colin@ki.tng.de>
2014-01-08 21:24:00 +0100

    add settings for custom alerts filters

--- pa_stats/scenes/global.js
+++ pa_stats/scenes/global.js
@@ -35,15 +35,118 @@ function unlockGame_() {
 	});
 }
 
+// parses all units, following unit bases recursively
+// onComplete is given the finished map of spec => custom piece of data per spec
+// dataGetter gets the data from the unit json, it expects one parameter: the parsed unit json
+// datamerger expected two parameters, the data further up the definition tree of the unit and further down
+// examples see the next 2 functions after this
+function loadUnitData(onComplete, dataGetter, dataMerger) {
+	var resultTypeMapping = {};
+	var spawnedUnitCalls = 0;
+	$.getJSON("coui://pa/units/unit_list.json", function(data) {
+		var units = data.units;
+		var finishedAll = false;
+		
+		function readUnitDataFromFile(file, callback) {
+			$.getJSON(file, function(unit) {
+				var freshDataFromUnit = dataGetter(unit);
+				var baseSpec = unit.base_spec;
+				
+				if (baseSpec != undefined) {
+					readUnitDataFromFile("coui://"+baseSpec, function(unitData) {
+						callback(dataMerger(freshDataFromUnit, unitData));
+					});
+				} else {
+					if (freshDataFromUnit != undefined) {
+						callback(freshDataFromUnit);
+					}
+					spawnedUnitCalls--;
+					if (spawnedUnitCalls == 0) {
+						onComplete(resultTypeMapping);
+					}
+				}
+			});
+		}
+		
+		spawnedUnitCalls = units.length;
+		for (var i = 0; i < units.length; i++) {
+			function processUnitPath(unitPath) {
+				readUnitDataFromFile("coui://"+unitPath, function(unitData) {
+					resultTypeMapping[unitPath] = unitData;
+				});
+			}
+			processUnitPath(units[i]);
+		}
+	});
+}
+
+function loadUnitTypesArray(onComplete) {
+	loadUnitTypeMapping(function(mapping) {
+		var types = [];
+		for (unit in mapping) {
+			types = types.concat(mapping[unit]);
+		}
+		types = types.filter(function(elem, pos) {
+			return types.indexOf(elem) == pos;
+		});
+		onComplete(types);
+	});
+}
+
+//creates a map of all unit specs to their display name
+function loadUnitNamesMapping(onComplete) {
+	loadUnitData(onComplete, function(unit) {
+		return unit.display_name;
+	}, function (dataUpTheTree, dataDownTheTree) {
+		return dataUpTheTree; // first name encountered is used
+	});
+}
+
+//creates a map of all unit spec to an array of their type
+function loadUnitTypeMapping(onComplete) {
+	loadUnitData(onComplete, function(unit) {
+		var unitTypes = unit.unit_types;
+		if (unitTypes != undefined) {
+			for (var u = 0; u < unitTypes.length; u++) {
+				unitTypes[u] = unitTypes[u].replace("UNITTYPE_", "");
+			}
+		}
+		return unitTypes;
+	}, function(dataUpTheTree, dataDownTheTree) {
+		if (dataUpTheTree == undefined) {
+			dataUpTheTree = [];
+		}
+		if (dataDownTheTree == undefined) {
+			dataDownTheTree = [];
+		}
+		return dataUpTheTree.concat(dataDownTheTree);
+	});
+}
+
 var nanodesu = "info.nanodesu.pastats.";
 var pa_stats_session_teams = nanodesu + "teams";
 var pa_stats_session_team_index = nanodesu + "team_index";
 var pa_stats_stored_version = nanodesu + "version";
 var wantsToSendKey = 'pa_stats_wants_to_send_';
 var showDataLiveKey = "pa_stats_show_data_live";
+var showCreatedAlerts = nanodesu + "showCreatedAlerts";
+var showDestroyedAlerts = nanodesu + "showDestroyedAlerts";
+var includedUnitSpecAlertsCreated = nanodesu + "includedUnitSpecAlerts.create";
+var excludedUnitSpecAlertsCreated = nanodesu + "excludedUnitSpecAlerts.create";
+var includedUnitSpecAlertsDestroyed = nanodesu + "includedUnitSpecAlerts.destroy";
+var excludedUnitSpecAlertsDestroyed = nanodesu + "excludedUnitSpecAlerts.destroy";
 
 // make sure the defaults are set
 if (localStorage[wantsToSendKey] === undefined) {
 	localStorage[wantsToSendKey] = encode(true);
 	localStorage[showDataLiveKey] = encode(true);	
+}
+// in an extra block of code, since this was added later on
+if (localStorage[showCreatedAlerts] === undefined) {
+	localStorage[showCreatedAlerts] = encode(["Factory"]);
+	localStorage[showDestroyedAlerts] = encode(["Structure"]);
+	localStorage[includedUnitSpecAlertsDestroyed] = encode([]);
+	localStorage[excludedUnitSpecAlertsDestroyed] = encode([]);
+	localStorage[includedUnitSpecAlertsCreated] = encode([]);
+	localStorage[excludedUnitSpecAlertsCreated] = encode([]);
 }
--- pa_stats/scenes/live_game.js
+++ pa_stats/scenes/live_game.js
@@ -148,6 +148,9 @@ handlers.server_state = function(m) {
 		unlockGame_();
 	}
 	switch(m.state) {
+		case 'landing':
+			pasHadReconnect = false;
+			break;
 		case 'game_over':
 			gameIsOverOrPlayerIsDead = true;
 			unlockGame_();
@@ -226,32 +229,81 @@ model.updateServerAndLocalTime();
 // hijack some method that is in the right place to execute our engine calls
 var paStatsOldApplyUiStuff = model.applyUIDisplaySettings;
 model.applyUIDisplaySettings = function() {
-	engine.call('watchlist.setCreationAlertTypes', JSON.stringify(['Mobile', 'Structure']));
-	engine.call('watchlist.setDeathAlertTypes', JSON.stringify(['Mobile', 'Structure']));
+	function listenToAllAlerts() {
+		engine.call('watchlist.setCreationAlertTypes', JSON.stringify(['Mobile', 'Structure']));
+		engine.call('watchlist.setDeathAlertTypes', JSON.stringify(['Mobile', 'Structure']));
+		// damage alerts trigger whenever health changes, even when it goes up.
+		// so this is kind of useless, thus no support for them is implemented
+//		engine.call('watchlist.setDamageAlertTypes', JSON.stringify(['Mobile', 'Structure']));
+	}
+	
+	listenToAllAlerts();
+	
+	// to get rid of wrong settings by rAlertsFilter
+	window.setTimeout(listenToAllAlerts, 3000);
+	// this basically is a race condition vs rAlertsFilter, so better save than sorry
+	window.setTimeout(listenToAllAlerts, 5000); 
+	
 	paStatsOldApplyUiStuff();
 };
 
 var pasCapturedEvents = [];
 
+var pasHadReconnect = true;
 var pasKnownIdLimit = undefined;
 var pasSeenConstructionEvents = {};
+
+var pasUnitTypeMapping = {};
+loadUnitTypeMapping(function(mapping) {
+	pasUnitTypeMapping = mapping;
+});
+
+var includeForCreationAlerts = decode(localStorage[includedUnitSpecAlertsCreated]);
+var excludeForCreationAlerts = decode(localStorage[excludedUnitSpecAlertsCreated]);
+var includeForDestroyAlerts = decode(localStorage[includedUnitSpecAlertsDestroyed]);
+var excludeForDestroyAlerts = decode(localStorage[excludedUnitSpecAlertsDestroyed]);
+var creationAlertForTypes = decode(localStorage[showCreatedAlerts]);
+var destroyAlertForTypes = decode(localStorage[showDestroyedAlerts]);
 
 var paStatsOldWatchListHandler = handlers.watch_list;
 handlers.watch_list = function(payload) {
 	function shouldBeVisibleAsAlert(notice) {
-		var creationAlertsFor = "Factory";
-	    var destructAlertsFor = "Structure";
-	    
-	    var checkFor = undefined;
+		var checkTypes = undefined;
+		var includeSpecs = undefined;
+		var exlcludeSpecs = undefined;
+		
 	    if (notice.watch_type == 0) { // created
-	    	checkFor = creationAlertsFor;
+	    	checkTypes = creationAlertForTypes;
+	    	includeSpecs = includeForCreationAlerts;
+	    	excludeSpecs = excludeForCreationAlerts;
 	    } else if (notice.watch_type == 2) { //  destroyed
-	    	checkFor = destructAlertsFor;
+	    	checkTypes = destroyAlertForTypes;
+	    	includeSpecs = includeForDestroyAlerts;
+	    	excludeSpecs = excludeForDestroyAlerts;
 	    } else {
 	    	return true; // damaged or something else we don't care for currently, so the alerts are unmodified anyway
 	    }
-	    // use the unit spec to type map to check if we are interested in that specific unit
-	    return $.inArray(checkFor, pasUnitTypeMapping[notice.spec_id]) != -1;
+	    
+	    function contains(ar, val) {
+	    	return $.inArray(val, ar) != -1;
+	    }
+	    
+	    if (contains(includeSpecs, notice.spec_id)) {
+	    	console.log("alert for inclusion "+notice.spec_id);
+	    	return true;
+	    } else if (contains(excludeSpecs, notice.spec_id)) {
+	    	console.log("no alert for exclusion "+notice.spec_id);
+	    	return false;
+	    } else {
+	    	for (var i = 0; i < checkTypes.length; i++) {
+	    		if (contains(pasUnitTypeMapping[notice.spec_id], checkTypes[i])) {
+	    			console.log("alert for type "+notice.spec_id);
+	    			return true;
+	    		}
+	    	}
+	    	console.log("no rule found for "+notice.spec_id);
+	    	return false; // nothing matched
+	    }
 	}
 	
 	if (mostRecentServerTime !== undefined) { // in this case we just can wait until we have the first time. Should only matter for reconnects
@@ -268,7 +320,7 @@ handlers.watch_list = function(payload) {
 					pasSeenConstructionEvents[notice.id] = true;
 				}
 				
-				if (notice.watch_type != 2 || pasKnownIdLimit >= notice.id || pasSeenConstructionEvents[notice.id]) {
+				if (notice.watch_type != 2 || (pasHadReconnect && pasKnownIdLimit >= notice.id) || pasSeenConstructionEvents[notice.id]) {
 					if (notice.watch_type == 2) {
 						delete pasSeenConstructionEvents[notice.id]; // prevent the set from growing forever
 					}
@@ -332,7 +384,7 @@ model.sendStats = function() {
 	var report = undefined;
 
 	if (gameLinkId === undefined) {
-		var report = new ReportData();
+		report = new ReportData();
 		
 		report.ident = gameIdent;
 		report.reporterUberName = uberName;
