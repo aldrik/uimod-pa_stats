8c6c8ae7b8820a87393fae9ca2eb13eef38520c4
ColaColin <cola_colin@ki.tng.de>
2014-05-23 20:51:13 +0200

    do not mention PTE for unknown builds

--- pa_stats/unknown_version/scenes/ranked_matcher/server_browser.js
+++ pa_stats/unknown_version/scenes/ranked_matcher/server_browser.js
@@ -2,7 +2,7 @@
 // gamma broke it, I hacked it to "work" again. This is somehow rather ugly...
 $('#sidebar').append('<a href="#" class="btn_std" style="width: 100%" data-bind="click_sound: \'default\', rollover_sound: \'default\' ">'+
 		'<div class="btn_label">'+
-		'    NO AUTOMATCHES IN THE PTE'+
+		'    NO AUTOMATCHES IN UNKNOWN BUILDS'+
 		'</div>'+
 		'<div style="margin-top: 8px; margin-right: 10px; font-size: 12px; float: right; display: none" id="pa_stats_players_note">You or somebody else<br/>is searching!</div>'+		
 		'</a>');
