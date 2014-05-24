eb861a065c91c9e18e2753f00a444f2a18b8b747
ColaColin <cola_colin@ki.tng.de>
2014-05-24 16:41:54 +0200

    fix settings

--- pa_stats/66567/scenes/settings.js
+++ pa_stats/66567/scenes/settings.js
@@ -1,7 +1,7 @@
 (function() {
-	$('#game_settings ul').append('<li class="game_settings"><a href="#tab_pa_stats">PA Stats</a></li>');
-	$('#game_settings').append("<div class='div_settings' id='tab_pa_stats'></div>");
-
+	 $("#main .header").children(":first").append("<li><a href='#tab_pa_stats' data-toggle='pill'>PA Stats</a></li>");
+     $("#main .content").children(":first").append('<div class="option-list tab-pane" id="tab_pa_stats"></div>');	
+	
 	function PaStatsSettingsModel() {
 		var self = this;
 		self.wantsToSend = ko.observable(decode(localStorage[paStatsGlobal.wantsToSendKey]));
@@ -10,15 +10,15 @@
 
 	var paStatsSettingsModel = new PaStatsSettingsModel();
 
-	var paStatsOldOk = model.ok;
-	model.ok = function() {
+	var paStatsOldOk = model.save;
+	model.save = function() {
 		paStatsOldOk();
 		localStorage[paStatsGlobal.wantsToSendKey] = encode(paStatsSettingsModel.wantsToSend());
 		localStorage[paStatsGlobal.showDataLiveKey] = encode(paStatsSettingsModel.showDataLive());
 	}
 
-	var paStatsOldDefaults = model.defaults;
-	model.defaults = function() {
+	var paStatsOldDefaults = model.restoreDefaults;
+	model.restoreDefaults = function() {
 		paStatsOldDefaults();
 		paStatsSettingsModel.wantsToSend(true);
 		paStatsSettingsModel.showDataLive(true);
