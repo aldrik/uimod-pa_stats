2d6e776adf1a3f61e049211275ce3c66e2b3ed1a
ColaColin <cola_colin@ki.tng.de>
2014-10-20 20:13:45 +0200

    new maps

--- pa_stats/scenes/ranked_matcher/maps/mappool.js
+++ pa_stats/scenes/ranked_matcher/maps/mappool.js
@@ -1,21 +1,417 @@
-var pa_stats_mappool = [
-
-{"name":"Systeem X Ranked", "description": "Classical moon 1vs1", "planets":[{"name":"Maan X","mass":5000,"position_x":21500,"position_y":100,"velocity_x":-0.7092908024787903,"velocity_y":152.49609375,"required_thrust_to_move":3,"starting_planet":true,"planet":{"seed":4400,"radius":500,"heightRange":25,"waterHeight":0,"temperature":0,"metalDensity":50,"metalClusters":0,"biomeScale":50,"biome":"moon"}}],"creator":"mot9001"},
-
-{"name":"Cross Back Ranked", "description": "Fighthing in circles" ,"planets":[{"name":"Cross Back","mass":10000,"position_x":19900,"position_y":100,"velocity_x":-0.7965277433395386,"velocity_y":158.5076446533203,"required_thrust_to_move":0,"starting_planet":true,"planet":{"seed":13853,"radius":500,"heightRange":12,"waterHeight":43,"temperature":72.99999237060547,"metalDensity":60.000003814697266,"metalClusters":30.000001907348633,"biomeScale":50,"biome":"lava"}}],"creator":"Wrongcat"},
-
-{"name":"Tug of War Ranked", "description": "One straight land strip to fight for","planets":[{"name":"Tug of War","mass":10000,"position_x":20100,"position_y":100,"velocity_x":-0.7846693396568298,"velocity_y":157.71714782714844,"required_thrust_to_move":0,"starting_planet":true,"planet":{"seed":18999,"radius":550,"heightRange":30.000003814697266,"waterHeight":46,"temperature":50,"metalDensity":70,"metalClusters":0,"biomeScale":50,"biome":"lava"}}],"creator":"elodea"},
-
-{"name":"Dustbowl Ranked", "description": "Some interesting chokepoints and map zoning","planets":[{"name":"Dustbowl","mass":10000,"position_x":20500,"position_y":0,"velocity_x":-0.000006826572189311264,"velocity_y":156.17376708984375,"required_thrust_to_move":0,"starting_planet":true,"planet":{"seed":9756,"radius":520,"heightRange":35,"waterHeight":27.999996185302734,"temperature":100,"metalDensity":60.000003814697266,"metalClusters":35,"biomeScale":75.00000006869588,"biome":"desert"}}],"creator":"elodea"},
-
-{"name":"Caledonia Ranked", "description": "Typical 1vs1, but with potential for navy","planets":[{"name":"Caledonia","mass":20000,"position_x":21100,"position_y":100,"velocity_x":-0.7295550107955933,"velocity_y":153.93467712402344,"required_thrust_to_move":0,"starting_planet":true,"planet":{"seed":11430,"radius":600,"heightRange":35,"waterHeight":43,"temperature":50,"metalDensity":40,"metalClusters":50,"biomeScale":50,"biome":"earth"}}],"creator":"[RLM]Max"},
-
-{"name":"Station 3 Ranked", "description": "standard ice cold 1v1 with 1 naval spawn, 1 coastal spawn","planets":[{"name":"Station 3","mass":10000,"position_x":19600,"position_y":0,"velocity_x":-0.000006981545084272511,"velocity_y":159.71913146972656,"required_thrust_to_move":0,"starting_planet":true,"planet":{"seed":10922,"radius":600,"heightRange":15.000001907348633,"waterHeight":44,"temperature":0.9999990463256836,"metalDensity":71,"metalClusters":0,"biomeScale":50,"biome":"earth"}}],"creator":"elodea"},
-
-{"name":"Water World Ranked", "description": "make sure to bring boats","planets":[{"name":"Boat Prime","mass":10000,"position_x":39900,"position_y":-9000,"velocity_x":24.327838897705078,"velocity_y":107.85343933105469,"required_thrust_to_move":0,"starting_planet":true,"planet":{"seed":21572,"radius":650,"heightRange":0,"waterHeight":198,"temperature":80,"metalDensity":72,"metalClusters":0,"biomeScale":50,"biome":"tropical"}}],"creator":"elodea"},
-
-{"name":"HoneyPot Ranked", "description": "Two identical planets orbiting an ice planet. Multiplanet spawns","planets":[{"name":"Planet Bob","mass":10150,"position_x":16900.0078125,"position_y":28399.99609375,"velocity_x":-105.70258331298828,"velocity_y":62.900508880615234,"required_thrust_to_move":0,"starting_planet":0,"planet":{"seed":32767,"radius":700,"heightRange":48.99999237060547,"waterHeight":0,"temperature":0,"metalDensity":70,"metalClusters":70,"biomeScale":50,"biome":"earth"}},{"name":"Masteroid","mass":5000,"position_x":23000,"position_y":24200,"velocity_x":-58.758033752441406,"velocity_y":131.0818634033203,"required_thrust_to_move":3,"starting_planet":1,"planet":{"seed":23470,"radius":450,"heightRange":0,"waterHeight":42,"temperature":100,"metalDensity":20,"metalClusters":20,"biomeScale":50,"biome":"lava"}},{"name":"SuperBiscuit","mass":5000,"position_x":10499.994140625,"position_y":32100.0078125,"velocity_x":-147.17190551757812,"velocity_y":-8.830135345458984,"required_thrust_to_move":3,"starting_planet":1,"planet":{"seed":23470,"radius":450,"heightRange":0,"waterHeight":42,"temperature":100,"metalDensity":20,"metalClusters":20,"biomeScale":50,"biome":"lava"}}],"creator":"PAG_Elodea"},
-
-{"name":"taxman 2V2 Ranked", "description": "Not only good for 2vs2 with potentially close spawns","planets":[{"name":"taxman2V2-5","mass":5000,"position_x":27400,"position_y":-8600,"velocity_x":39.51422882080078,"velocity_y":125.89418029785156,"required_thrust_to_move":0,"starting_planet":true,"planet":{"seed":5977,"radius":600,"heightRange":25,"waterHeight":45,"temperature":50,"metalDensity":85,"metalClusters":5,"biomeScale":50,"biome":"lava"}}],"creator":"Taxman"},
-
-];
+var pa_stats_mappool = [ {
+	"name" : "Dust of Doom RANKED",
+	"planets" : [ {
+		"name" : "Sands of Death",
+		"mass" : 30275,
+		"position_x" : 13100,
+		"position_y" : 3100,
+		"velocity_x" : 44.380496978759766,
+		"velocity_y" : -187.54335021972656,
+		"required_thrust_to_move" : 0,
+		"starting_planet" : true,
+		"planet" : {
+			"seed" : 3348,
+			"radius" : 550,
+			"heightRange" : 25,
+			"waterHeight" : 40,
+			"temperature" : 100,
+			"metalDensity" : 47,
+			"metalClusters" : 70,
+			"biomeScale" : 75.00000006869588,
+			"biome" : "desert",
+			"numArmies" : 2,
+			"landingZonesPerArmy" : 2,
+			"landingZoneSize" : 200
+		}
+	} ],
+	"creator" : "Team Genesis"
+}, {
+	"name" : "taxman 2V2 RANKED",
+	"planets" : [ {
+		"name" : "taxman2V2-5",
+		"mass" : 5000,
+		"position_x" : 27400,
+		"position_y" : -8600,
+		"velocity_x" : 39.51422882080078,
+		"velocity_y" : 125.89418029785156,
+		"required_thrust_to_move" : 0,
+		"starting_planet" : true,
+		"planet" : {
+			"seed" : 5977,
+			"radius" : 600,
+			"heightRange" : 25,
+			"waterHeight" : 45,
+			"temperature" : 50,
+			"metalDensity" : 100,
+			"metalClusters" : 5,
+			"biomeScale" : 50,
+			"biome" : "lava",
+			"numArmies" : 2,
+			"landingZonesPerArmy" : 1,
+			"landingZoneSize" : 150
+		}
+	} ],
+	"creator" : "Team Genesis"
+}, {
+	"name" : "Tug of War RANKED",
+	"planets" : [ {
+		"name" : "Tug of War",
+		"mass" : 10000,
+		"position_x" : 20100,
+		"position_y" : 100,
+		"velocity_x" : -0.7846693396568298,
+		"velocity_y" : 157.71714782714844,
+		"required_thrust_to_move" : 0,
+		"starting_planet" : true,
+		"planet" : {
+			"seed" : 18999,
+			"radius" : 596,
+			"heightRange" : 30.000003814697266,
+			"waterHeight" : 40,
+			"temperature" : 50,
+			"metalDensity" : 71,
+			"metalClusters" : 13,
+			"biomeScale" : 50,
+			"biome" : "lava",
+			"numArmies" : 2,
+			"landingZonesPerArmy" : 0,
+			"landingZoneSize" : 0
+		}
+	} ],
+	"creator" : "Team Genesis"
+}, {
+	"name" : "Cross Back RANKED",
+	"planets" : [ {
+		"name" : "Cross Back",
+		"mass" : 10000,
+		"position_x" : 19800,
+		"position_y" : 100,
+		"velocity_x" : -0.802569568157196,
+		"velocity_y" : 158.90740966796875,
+		"required_thrust_to_move" : 0,
+		"starting_planet" : true,
+		"planet" : {
+			"seed" : 13853,
+			"radius" : 500,
+			"heightRange" : 12,
+			"waterHeight" : 43,
+			"temperature" : 72.99999237060547,
+			"metalDensity" : 50,
+			"metalClusters" : 35,
+			"biomeScale" : 50,
+			"biome" : "lava",
+			"numArmies" : 2,
+			"landingZonesPerArmy" : 2,
+			"landingZoneSize" : 150
+		}
+	} ],
+	"creator" : "Team Genesis"
+}, {
+	"name" : "Water World RANKED",
+	"planets" : [ {
+		"name" : "Boat Prime",
+		"mass" : 10000,
+		"position_x" : 39900,
+		"position_y" : -9000,
+		"velocity_x" : 24.327838897705078,
+		"velocity_y" : 107.85343933105469,
+		"required_thrust_to_move" : 0,
+		"starting_planet" : true,
+		"planet" : {
+			"seed" : 21572,
+			"radius" : 650,
+			"heightRange" : 0,
+			"waterHeight" : 198,
+			"temperature" : 80,
+			"metalDensity" : 81,
+			"metalClusters" : 0,
+			"biomeScale" : 50,
+			"biome" : "tropical",
+			"numArmies" : 2,
+			"landingZonesPerArmy" : 0,
+			"landingZoneSize" : 0
+		}
+	} ],
+	"creator" : "Team Genesis"
+}, {
+	"name" : "Dustbowl RANKED",
+	"planets" : [ {
+		"name" : "Dustbowl",
+		"mass" : 10000,
+		"position_x" : 20500,
+		"position_y" : 0,
+		"velocity_x" : -0.000006826572189311264,
+		"velocity_y" : 156.17376708984375,
+		"required_thrust_to_move" : 0,
+		"starting_planet" : true,
+		"planet" : {
+			"seed" : 9756,
+			"radius" : 520,
+			"heightRange" : 35,
+			"waterHeight" : 27.999996185302734,
+			"temperature" : 100,
+			"metalDensity" : 58.999996185302734,
+			"metalClusters" : 56,
+			"biomeScale" : 75.00000006869588,
+			"biome" : "desert",
+			"numArmies" : 2,
+			"landingZonesPerArmy" : 0,
+			"landingZoneSize" : 0
+		}
+	} ],
+	"creator" : "Team Genesis"
+}, {
+	"name" : "HoneyPot RANKED",
+	"planets" : [ {
+		"name" : "Planet Bob",
+		"mass" : 10150,
+		"position_x" : 16900.0078125,
+		"position_y" : 28399.99609375,
+		"velocity_x" : -105.70258331298828,
+		"velocity_y" : 62.900508880615234,
+		"required_thrust_to_move" : 0,
+		"starting_planet" : false,
+		"planet" : {
+			"seed" : 32767,
+			"radius" : 700,
+			"heightRange" : 48.99999237060547,
+			"waterHeight" : 0,
+			"temperature" : 0,
+			"metalDensity" : 100,
+			"metalClusters" : 70,
+			"biomeScale" : 50,
+			"biome" : "earth",
+			"numArmies" : 2,
+			"landingZonesPerArmy" : 0,
+			"landingZoneSize" : 0
+		}
+	}, {
+		"name" : "Masteroid",
+		"mass" : 5000,
+		"position_x" : 23000,
+		"position_y" : 24200,
+		"velocity_x" : -58.758033752441406,
+		"velocity_y" : 131.0818634033203,
+		"required_thrust_to_move" : 3,
+		"starting_planet" : true,
+		"planet" : {
+			"seed" : 23470,
+			"radius" : 450,
+			"heightRange" : 0,
+			"waterHeight" : 42,
+			"temperature" : 100,
+			"metalDensity" : 4,
+			"metalClusters" : 8,
+			"biomeScale" : 50,
+			"biome" : "lava",
+			"numArmies" : 2,
+			"landingZonesPerArmy" : 0,
+			"landingZoneSize" : 0
+		}
+	}, {
+		"name" : "SuperBiscuit",
+		"mass" : 5000,
+		"position_x" : 10499.994140625,
+		"position_y" : 32100.0078125,
+		"velocity_x" : -147.17190551757812,
+		"velocity_y" : -8.830135345458984,
+		"required_thrust_to_move" : 3,
+		"starting_planet" : true,
+		"planet" : {
+			"seed" : 23470,
+			"radius" : 450,
+			"heightRange" : 0,
+			"waterHeight" : 42,
+			"temperature" : 100,
+			"metalDensity" : 4,
+			"metalClusters" : 8,
+			"biomeScale" : 50,
+			"biome" : "lava",
+			"numArmies" : 2,
+			"landingZonesPerArmy" : 0,
+			"landingZoneSize" : 0
+		}
+	} ],
+	"creator" : "Team Genesis"
+}, {
+	"name" : "Station 3 RANKED",
+	"planets" : [ {
+		"name" : "Station 3",
+		"mass" : 10000,
+		"position_x" : 19600,
+		"position_y" : 0,
+		"velocity_x" : -0.000006981545084272511,
+		"velocity_y" : 159.71913146972656,
+		"required_thrust_to_move" : 0,
+		"starting_planet" : true,
+		"planet" : {
+			"seed" : 10922,
+			"radius" : 600,
+			"heightRange" : 20,
+			"waterHeight" : 42,
+			"temperature" : 0.9999990463256836,
+			"metalDensity" : 82,
+			"metalClusters" : 46,
+			"biomeScale" : 50,
+			"biome" : "earth",
+			"numArmies" : 2,
+			"landingZonesPerArmy" : 0,
+			"landingZoneSize" : 0
+		}
+	} ],
+	"creator" : "Team Genesis"
+}, {
+	"name" : "Clarke RANKED",
+	"planets" : [ {
+		"name" : "Clarke",
+		"mass" : 10000,
+		"position_x" : 12900,
+		"position_y" : -24300,
+		"velocity_x" : 119.072998046875,
+		"velocity_y" : 63.21149826049805,
+		"required_thrust_to_move" : 0,
+		"starting_planet" : true,
+		"planet" : {
+			"seed" : 19387,
+			"radius" : 900,
+			"heightRange" : 22,
+			"waterHeight" : 40,
+			"temperature" : 100,
+			"metalDensity" : 94,
+			"metalClusters" : 11,
+			"biomeScale" : 75.00000006869588,
+			"biome" : "desert",
+			"numArmies" : 2,
+			"landingZonesPerArmy" : 0,
+			"landingZoneSize" : 0
+		}
+	} ],
+	"creator" : "Team Genesis"
+}, {
+	"name" : "Systeem X RANKED",
+	"planets" : [ {
+		"name" : "Maan X",
+		"mass" : 5000,
+		"position_x" : 21500,
+		"position_y" : 100,
+		"velocity_x" : -0.7092908024787903,
+		"velocity_y" : 152.49609375,
+		"required_thrust_to_move" : 3,
+		"starting_planet" : true,
+		"planet" : {
+			"seed" : 22222,
+			"radius" : 500,
+			"heightRange" : 25,
+			"waterHeight" : 0,
+			"temperature" : 0,
+			"metalDensity" : 60.000003814697266,
+			"metalClusters" : 25,
+			"biomeScale" : 50,
+			"biome" : "moon",
+			"numArmies" : 2,
+			"landingZonesPerArmy" : 0,
+			"landingZoneSize" : 0
+		}
+	} ],
+	"creator" : "Team Genesis"
+}, {
+	"name" : "Capital N RANKED",
+	"planets" : [ {
+		"name" : "Block",
+		"mass" : 35000,
+		"position_x" : 19900,
+		"position_y" : 0,
+		"velocity_x" : -0.000006928721177246189,
+		"velocity_y" : 158.5106658935547,
+		"required_thrust_to_move" : 0,
+		"starting_planet" : true,
+		"planet" : {
+			"seed" : 9001,
+			"radius" : 600,
+			"heightRange" : 0,
+			"waterHeight" : 0,
+			"temperature" : 50,
+			"metalDensity" : 68,
+			"metalClusters" : 31,
+			"biomeScale" : 50,
+			"biome" : "metal",
+			"numArmies" : 2,
+			"landingZonesPerArmy" : 1,
+			"landingZoneSize" : 0
+		}
+	} ],
+	"creator" : "Team Genesis"
+}, {
+	"name" : "Faceoff RANKED",
+	"planets" : [ {
+		"name" : "Singopalis",
+		"mass" : 35000,
+		"position_x" : -10132.1513671875,
+		"position_y" : 7490.6259765625,
+		"velocity_x" : -118.42047882080078,
+		"velocity_y" : -160.1808319091797,
+		"required_thrust_to_move" : 0,
+		"starting_planet" : true,
+		"planet" : {
+			"seed" : 9001,
+			"radius" : 500,
+			"heightRange" : 0,
+			"waterHeight" : 0,
+			"temperature" : 50,
+			"metalDensity" : 100,
+			"metalClusters" : 6,
+			"biomeScale" : 50,
+			"biome" : "metal",
+			"numArmies" : 2,
+			"landingZonesPerArmy" : 1,
+			"landingZoneSize" : 0
+		}
+	}, {
+		"name" : "Armstrong",
+		"mass" : 35000,
+		"position_x" : -8200,
+		"position_y" : -11300,
+		"velocity_x" : -153.1632080078125,
+		"velocity_y" : 111.14497375488281,
+		"required_thrust_to_move" : 0,
+		"starting_planet" : true,
+		"planet" : {
+			"seed" : 9001,
+			"radius" : 500,
+			"heightRange" : 0,
+			"waterHeight" : 0,
+			"temperature" : 50,
+			"metalDensity" : 100,
+			"metalClusters" : 6,
+			"biomeScale" : 50,
+			"biome" : "metal",
+			"numArmies" : 2,
+			"landingZonesPerArmy" : 1,
+			"landingZoneSize" : 0
+		}
+	}, {
+		"name" : "Pebble",
+		"mass" : 5000,
+		"position_x" : 50000,
+		"position_y" : 0,
+		"velocity_x" : 0.0000043711384023481514,
+		"velocity_y" : -100,
+		"required_thrust_to_move" : 2,
+		"starting_planet" : false,
+		"planet" : {
+			"seed" : 9001,
+			"radius" : 270,
+			"heightRange" : 99.00000762939453,
+			"waterHeight" : 0,
+			"temperature" : 0,
+			"metalDensity" : 0,
+			"metalClusters" : 0,
+			"biomeScale" : 50,
+			"biome" : "moon",
+			"numArmies" : 2,
+			"landingZonesPerArmy" : 0,
+			"landingZoneSize" : 0
+		}
+	} ],
+	"creator" : "Team Genesis"
+} ];
