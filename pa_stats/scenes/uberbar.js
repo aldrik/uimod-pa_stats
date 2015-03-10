d194687e219a5645970fe1af9a90c5ee419cc186
ColaColin <cola_colin@ki.tng.de>
2015-03-10 21:07:28 +0100

    add some debug logs

--- pa_stats/scenes/uberbar.js
+++ pa_stats/scenes/uberbar.js
@@ -4,6 +4,8 @@
 	var oldOnCommand = model.onCommand;
 	model.onCommand = function(uberid, command) {
 		try {
+			console.log("oncommand called");
+			console.log(command);
 			if (command.message_type === 'game_lobby_info' && model.acceptedGameInviteFrom() === uberid) {
 				localStorage['lobbyId'] = encode(command.payload.lobby_id);
 				localStorage[paStatsGlobal.isLocalGame] = encode(command.payload.local_game === true);
