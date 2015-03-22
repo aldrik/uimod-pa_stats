81c95471e1cc1bd1e8ce4bba24a23a68abef6698
ColaColin <cola_colin@ki.tng.de>
2015-03-22 14:01:28 +0100

    better protection against big map data parts

--- pa_stats/scenes/new_game.js
+++ pa_stats/scenes/new_game.js
@@ -1,12 +1,48 @@
 (function() {
 	localStorage[paStatsGlobal.isRankedGameKey] = encode(false);
 	
-	var setCapturedSystem = function(v) {
-		for (var i = 0; i < v.planets.length; i++) {
-			delete v.planets[i].planetCSG; // until the pa stats server handles the big parts more effective don't send them to pa stats :/
-			delete v.planets[i].metal_spots;
+	// until the pa stats server handles the big parts more effective don't send them to pa stats :/
+	var copyRelevantSystemInfo = function(sys) {
+		var result = {};
+		result.name = sys.name;
+		
+		result.planets = [];
+		
+		if (sys.planets) {
+			for (var i = 0; i < sys.planets.length; i++) {
+				var planet = sys.planets[i];
+				if (planet) {
+					var cp = {};
+					cp.name = planet.name;
+					cp.required_thrust_to_move = planet.required_thrust_to_move;
+					cp.starting_planet = planet.starting_planet;
+					cp.mass = planet.mass;
+					cp.position_x = planet.position_x;
+					cp.position_y = planet.position_y;
+					cp.velocity_x = planet.velocity_x;
+					cp.velocity_y = planet.velocity_y;
+					cp.planet = {};
+					if (planet.planet) {
+						cp.planet.temperature = planet.planet.temperature;
+						cp.planet.seed = planet.planet.seed;
+						cp.planet.radius = planet.planet.radius;
+						cp.planet.biome = planet.planet.biome;
+						cp.planet.waterHeight = planet.planet.waterHeight;
+						cp.planet.heightRange = planet.planet.heightRange;
+						cp.planet.waterDepth = planet.planet.waterDepth;
+						cp.planet.metalDensity = planet.planet.metalDensity;
+						cp.planet.biomeScale = planet.planet.biomeScale;
+						cp.planet.metalClusters = planet.planet.metalClusters;
+					}
+					result.planets.push(cp);
+				}
+			}
 		}
-		localStorage['pa_stats_loaded_planet_json'] = JSON.stringify(v);
+		return result;
+	};
+	
+	var setCapturedSystem = function(v) {
+		localStorage['pa_stats_loaded_planet_json'] = JSON.stringify(copyRelevantSystemInfo(v));
 	};
 
 	var oldSystemHandler = handlers.system;
