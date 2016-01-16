550706ffc0500aaa4ea8e22b1ab417085fca31a3
MikeyH <mikeyh@users.noreply.github.com>
2016-01-17 07:30:49 +1300

    Optimise and clean up loading of PA Chat
    - only add PA Chat scenes if needed to prevent unnecessary loading
    - added new PA Chat scenes
    - disable ajax (no longer needed - still need to clean up settings)

--- pa_stats/pa_stats_loader.js
+++ pa_stats/pa_stats_loader.js
@@ -13,35 +13,60 @@ var paStatsBaseDir = typeof statsDevelopmentNeverUseThisNameAnywhereElseIDareYou
 		}
 	}
 	
-	var getCurrentSceneName = function() {
-		return window.location.pathname.split("/").pop().replace(".html", "");
-	};
+	var currentSceneName = window.location.pathname.split("/").pop().replace(".html", "");
 	
-	console.log("determined current scene name to be " + getCurrentSceneName());
+	console.log( 'PA Stats current scene is ' + currentSceneName );
 	
 	var b = paStatsBaseDir;
 	
 	// due to the ordering of things this needs to be loaded where scene mods are loaded, but for all scenes.
-	addSceneEntry(getCurrentSceneName(), b+'lib/ajax_ubernet.js');
-	
+//	addSceneEntry(currentSceneName, b+'lib/ajax_ubernet.js');
+
 	// PA Chat
-	addSceneEntry('uberbar', "https://dfpsrd4q7p23m.cloudfront.net/mods/gOptimiseUserTagMap/ui/mods/gOptimiseUserTagMap/uberbar.js");
-	addSceneEntry('uberbar', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/jabber.js");
-	addSceneEntry('uberbar', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/uberbar.css");
-	addSceneEntry('uberbar', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/uberbar.js");
 	
-	addSceneEntry('live_game_options_bar', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/live_game_options_bar.css");
-	addSceneEntry('live_game_options_bar', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/live_game_options_bar.js");
-
-	addSceneEntry('start', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/start.js");
-	addSceneEntry('matchmaking', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/matchmaking.js");
-	addSceneEntry('new_game_ladder', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/new_game_ladder.js");
-	addSceneEntry('server_browser', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/server_browser.js");
-	addSceneEntry('new_game', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/new_game.js");
-	addSceneEntry('live_game', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/live_game.js");
-	addSceneEntry('system_editor', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/system_editor.js");
-
+	var paChatLoaded = false;
+	
+	_.forEach( scene_mod_list.uberbar, function( entry )
+	{
+		if ( entry.indexOf( '/mods/pachat/uberbar.js' ) != -1 )
+		{
+			paChatLoaded = true;
+			return false;
+		}
+	});
+	
+	if ( paChatLoaded )
+	{
+		if ( currentSceneName == 'uberbar' )
+		{
+			console.log( 'PA Chat already loaded before PA Stats' );
+		}
+	}
+	else
+	{
+		addSceneEntry('uberbar', "https://dfpsrd4q7p23m.cloudfront.net/mods/gOptimiseUserTagMap/ui/mods/gOptimiseUserTagMap/uberbar.js");
+		addSceneEntry('uberbar', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/jabber.js");
+		addSceneEntry('uberbar', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/uberbar.css");
+		addSceneEntry('uberbar', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/uberbar.js");
+		
+		addSceneEntry('live_game_options_bar', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/live_game_options_bar.css");
+		addSceneEntry('live_game_options_bar', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/live_game_options_bar.js");
+	
+		addSceneEntry('start', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/start.js");
+		addSceneEntry('matchmaking', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/matchmaking.js");
+		addSceneEntry('leaderboard', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/leaderboard.js");
+		addSceneEntry('new_game_ladder', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/new_game_ladder.js");
+		addSceneEntry('server_browser', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/server_browser.js");
+		addSceneEntry('new_game', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/new_game.js");
+		addSceneEntry('live_game', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/live_game.js");
+		addSceneEntry('replay_browser', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/replay_browser.js");
+		addSceneEntry('system_editor', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/system_editor.js");
+		addSceneEntry('load_planet', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/load_planet.js");
+		addSceneEntry('settings', "https://dfpsrd4q7p23m.cloudfront.net/mods/pachat/settings.js");
+	}
+	
 	// End of PA Chat
+
 	
 	if (typeof paStatsGlobal === 'undefined') {
 		loadScript(b+'lib/unitInfoParser.js');
