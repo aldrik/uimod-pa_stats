496e44e32f6f6d230be3c88ecd29d51211297e2b
MikeyH <mikeyh@users.noreply.github.com>
2016-04-13 03:38:25 +1200

    Reorganise and cleanup files

--- pa_stats/scenes/connect.js
+++
@@ -1,8 +0,0 @@
-(function() {
-	var oldStartGame = api.net.startGame;
-	api.net.startGame = function(region, mode) {
-		var r = oldStartGame(region, mode);
-		localStorage[paStatsGlobal.isLocalGame] = encode(region === 'Local');
-		return r;
-	};
-}());
--- pa_stats/scenes/matchmaking.js
+++
@@ -1,7 +0,0 @@
-(function() {
-	var oldJoinGame = model.joinGame;
-	model.joinGame = function(lobbyId) {
-		localStorage['lobbyId'] = encode(lobbyId);
-		return oldJoinGame(lobbyId);
-	};
-}());
